c51f3bdb642e5a4d90f9689efb71f21ef8313d7d
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2019-07-12 15:28:53 -0700

    added spatial stats domain and plotting

--- Landis_spatial.py
+++ Landis_spatial.py
@@ -6,12 +6,14 @@
 # email: ehsan.mosadegh@gmail.com, ehsanm@dri.edu
 # notes:
 ############################################################
+# import libraries
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import time
+from netCDF4 import Dataset
 
 #===========================================================
 # get the starting time
@@ -21,37 +23,86 @@ start = time.time()
 #===========================================================
 # controlling options:
 
+# set the resolution of spatial plot
+spatial_res= 'i' # 'l' , 'i' , 'f'
+# set output format
+plot_format='png'  # 'png' 'svg'
 # select the domain
-my_domain= 'cmaq_domain' # 'zoomed_domain' or 'cmaq_domain' 
+my_domain= 'zoomed_domain' # 'zoomed_domain' or 'cmaq_domain'
 # save the plot or not?
-save_plot= 'no'  # 'yes' or 'no'
+save_plot= 'yes'  # 'yes' or 'no'
 # select scenario number
 scenario_no = '5'
 # fsize=9  # font-size
 month_list = [ 'jul' , 'aug' , 'sep' , 'oct' , 'nov']
-# set the resolution of spatial plot
-spatial_res= 'l' # 'l' , 'i' , 'f'
-plot_format='pdf'  # 'png' 'svg'
+
+# define what type of spatial plot
+spatial_plot_type='mesh' # 'fires' or 'marker' or 'mesh'
+
+mesh_type='region_for_stats' # 'sample_mesh' or 'modeling_domain' or 'station_inside_cell' or 'region_for_stats'
+
+#===========================================================
+# Basemap plot setting
+
+zoomed_domain_zoomOut_scale_factor=10
+cmaq_domain_zoomOut_scale_factor=1
+mesh_domain_range=15
+
+### set the station location
+stn_lon=-119.9772
+stn_lat=38.9399
+
+### for a locatio
+marker_lon=stn_lon		#-120.0324
+marker_lat=stn_lat		#39.0968
+
+### for statistics mesh
+stats_mesh_ll_lon=-120.1
+stats_mesh_ll_lat=38.83
+
+### center of CMAQ domain
+xcent_cmaq=-120.806 # degrees, ref_lon from WPS namelist
+ycent_cmaq=38.45 # degrees, ref_lat from WPS namelist
+
+### center of my desired map== I like to set Lake Tahoe at the center
+lon_of_desired_center=marker_lon		# center of the map; degrees
+lat_of_desired_center=marker_lat		# center of the map; degrees
+
+# # lower-left corner of desired map, which I set it based on CMAQ LATD, LOND
+# lower_left_lon_list_of_fires=-122.0 # lower-left corner of the map; degrees, -120.1407
+# lower_left_lon_list_of_fires=20.0# lower-left corner of the map; degrees, 37.60086
+
+### domain size  for zoomed map
+NROWS = 265*1000*cmaq_domain_zoomOut_scale_factor # height of the map; meters
+NCOLS = 250*1000*cmaq_domain_zoomOut_scale_factor #
+
+### domain size  for zoomed map
+NROWS_zoom = 10000*zoomed_domain_zoomOut_scale_factor # height of the map; meters
+NCOLS_zoom = 10000*zoomed_domain_zoomOut_scale_factor # width of the map; meters
+
 # other settinggs
 scenario_year = 30
 
 print(f'-> save plot is= {save_plot} ')
-print(f'-> plot domain is set to= {my_domain}')
+print(f'-> plot domain is set to= {my_domain} ')
+print(f'-> resolution is= ({spatial_res}) ')
+print(f'-> spatial plot type is= {spatial_plot_type}')
+
 #===========================================================
 # define the input file
 
-input_file_name = 'Scenario_'+scenario_no+'_year_30_latlon.csv' 
+grid_dot_file_name = 'Scenario_'+scenario_no+'_year_30_latlon.csv'
 
-input_file_path = '/Users/ehsan/Documents/Python_projects/USFS_fire/inputs/landis_inputs/landis_input_files_latlon_converted/' # '/' at the end of the path
+grid_dot_file_path = '/Users/ehsan/Documents/Python_projects/USFS_fire/inputs/landis_inputs/landis_input_files_latlon_converted/' # '/' at the end of the path
 
-input_file_full_path = input_file_path + input_file_name
+grid_dot_file_full_path = grid_dot_file_path + grid_dot_file_name
 
 #===========================================================
 # read-in data
 
 print(f'-> reading input file for scenario= {scenario_no}')
 
-input_df = pd.read_csv( input_file_full_path , sep=',' , header=0 )#, names= ColumnList)# ,  index_listcol=0 ) why index_listcol does not work?
+input_df = pd.read_csv( grid_dot_file_full_path , sep=',' , header=0 )#, names= ColumnList)# ,  index_listcol=0 ) why index_listcol does not work?
 
 #===========================================================
 # filter zero jdays
@@ -61,27 +112,7 @@ filter_nonZero_days = ( input_df['FireDay-30'] != 0 )
 input_df_nonZero_days = input_df[ filter_nonZero_days ]
 
 #===========================================================
-# Basemap plot setting to zoom
-
-### center of CMAQ domain
-xcent_cmaq =-120.806 # degrees
-ycent_cmaq =40.0 # degrees
-
-# center of my desired map== I like to set Lake Tahoe at the center
-lon_desired_cent =-120.0323507 # center of the map; degrees
-lat_desired_cent =39.02 # center of the map; degrees
-
-# # lower-left corner of desired map, which I set it based on CMAQ LATD, LOND 
-# lower_left_lon_list_of_fires=-122.0 # lower-left corner of the map; degrees, -120.1407
-# lower_left_lon_list_of_fires=20.0# lower-left corner of the map; degrees, 37.60086 
-
-### domain size  for zoomed map
-NROWS = 500*1000 # height of the map; meters
-NCOLS = 500*1000 #
-
-### domain size  for zoomed map
-NROWS_zoom = 70000 # height of the map; meters
-NCOLS_zoom = 65000 # width of the map; meters
+# basemap 
 
 # # upper-right corner
 # upper_right_lon_list_of_fires=-118.0 # meters
@@ -90,18 +121,18 @@ print(" ")
 print('-> making the map now ...')
 
 # draw the map background --> map of whole domain
-# my_desired_base_map= Basemap(projection='lcc' ,\
+# desired_underlying_map= Basemap(projection='lcc' ,\
 # 	llcrnrx=lower_left_lon_list_of_fires , llcrnry=lower_left_lon_list_of_fires ,\
 # 	lon_list_of_fires_0=lon_list_of_fires_desired_cent , lon_list_of_fires_0=lon_list_of_fires_desired_cent ,\
 # 	height=NROWS_zoom , width=NCOLS_zoom ,\
 # 	 resolution='l' , area_thresh=0.5) # urcrnrx=upper_right_lon_list_of_fires , urcrnry=upper_right_lon_list_of_fires
 
-
 # first, we plot a desired base-map, and then we plot our data on this map
 if (my_domain=='cmaq_domain'):
 	print(f'-> domain is= {my_domain}')
+	print(" ")
 
-	my_desired_base_map= Basemap(projection='lcc' ,\
+	desired_underlying_map= Basemap(projection='lcc' ,\
 		lat_0=ycent_cmaq , lon_0=xcent_cmaq ,\
 		height=NROWS , width=NCOLS ,\
 		resolution=spatial_res , area_thresh=0.5)
@@ -109,79 +140,222 @@ if (my_domain=='cmaq_domain'):
 # new version of my map --> zoomed map
 if(my_domain=='zoomed_domain'):
 	print(f'-> domain is= {my_domain}')
+	print(" ")
 
-	my_desired_base_map= Basemap(projection='lcc' ,\
-		lat_0=lat_desired_cent , lon_0=lon_desired_cent ,\
+	desired_underlying_map= Basemap(projection='lcc' ,\
+		lat_0=lat_of_desired_center , lon_0=lon_of_desired_center ,\
 		height=NROWS_zoom , width=NCOLS_zoom ,\
 		resolution=spatial_res , area_thresh=0.5) # 	urcrnrlon_list_of_fires=upper_right_lon_list_of_fires , urcrnrlon_list_of_fires=upper_right_lon_list_of_fires,\ , llcrnrlon_list_of_fires=lower_left_lon_list_of_fires , llcrnrlon_list_of_fires=lower_left_lon_list_of_fires ,\
 
-my_desired_base_map.fillcontinents( color='#CCCCCC' , lake_color='lightblue' , zorder=1 ) # , 
-my_desired_base_map.bluemarble( zorder=1 )
-#my_desired_base_map.etopo( zorder=1 )
-#my_desired_base_map.shadedrelief( zorder=1 )
 
-my_desired_base_map.drawmapboundary(color='k' )#, fill_color='#46bcec' ) #, fill_color='aqua')
-my_desired_base_map.drawcoastlines(color = '0.15' , zorder=2 )
-my_desired_base_map.drawcounties(linewidth=0.5 , color='k' , zorder=3 )
-my_desired_base_map.drawstates(zorder=4)
+### set the background type of the basemap with zorder=1
+desired_underlying_map.fillcontinents( color='#CCCCCC' , lake_color='lightblue' , zorder=1 ) # ,
+#desired_underlying_map.bluemarble( zorder=1 )
+#desired_underlying_map.etopo( zorder=1 )
+#desired_underlying_map.shadedrelief( zorder=1 )
+
+### draw other map characteristics
+desired_underlying_map.drawmapboundary(color='k' )#, fill_color='#46bcec' ) #, fill_color='aqua')
+desired_underlying_map.drawcoastlines(color = '0.15' , zorder=2 )
+desired_underlying_map.drawcounties(linewidth=0.5 , color='k' , zorder=3 )
+desired_underlying_map.drawstates(zorder=4 )
 
 
 print(" ")
 #===========================================================
-# define a dictionary for 
-month_dict = {
+# make plots for different spatial plot types
 
-	'jul' : [ 183 , 213 , 'v'] ,
-	'aug' : [ 214 , 244 , 'p'] ,
-	'sep' : [ 245 , 274 , '^'] ,
-	'oct' : [ 275 , 305 , '.'] ,
-	'nov' : [ 306 , 335 , 'd'] 
+if ( spatial_plot_type == 'fires' ) :
 
-	}
+	# define a dictionary for
+	month_dict = {
+
+		'jul' : [ 183 , 213 , 'v'] ,
+		'aug' : [ 214 , 244 , 'p'] ,
+		'sep' : [ 245 , 274 , '^'] ,
+		'oct' : [ 275 , 305 , '.'] ,
+		'nov' : [ 306 , 335 , 'd']
+
+		}
+
+	# loop through months and and filter chunks
+
+	for month in month_list :
+
+		print( f'-> processing month= {month}' )
+
+		filter_month = ( input_df_nonZero_days[ 'FireDay-30' ] >= month_dict[month][0] ) & ( input_df_nonZero_days[ 'FireDay-30' ] <= month_dict[month][1] )
+
+		chunck_of_df = input_df_nonZero_days [ filter_month ]
+
+		lat_list_of_fires = chunck_of_df ['Lat'].values
+		print(f'-> no. of burned pixels in {month} in scen ({scenario_no})= {len(lat_list_of_fires)}')
+		#print(lon_list_of_fires)
+
+		lon_list_of_fires = chunck_of_df ['Long'].values
+		#print(f'-> no. of fires in {month} = {len(lon_list_of_fires)}')
+
+		#x_coord , y_coord = desired_underlying_map( lon_list_of_fires , lon_list_of_fires ) # order: x ,y; degrees to meters
+
+		#desired_underlying_map.scatter( x_coord , y_coord , latlon=True , marker= month_dict[month][2] , s=10 , label=month) # If lon_list_of_fireslon_list_of_fires is False (default), x and y are assumed to be in map projection coordinates.
+	#source: https://matplotlib.org/basemap/api/basemap_api.html
+
+		desired_underlying_map.scatter( lon_list_of_fires , lat_list_of_fires , latlon=True , marker= month_dict[month][2] , s=12 , label=month ,  zorder=5 ) # If lon_list_of_fireslon_list_of_fires is False (default), x and y are assumed to be in map projection coordinates.
+
+	#desired_underlying_map.etopo(scale=0.5, alpha=0.5)  # the map backgroud
+	#desired_underlying_map.shadedrelief(scale=0.5)
+	plt.legend( scatterpoints=1 , frameon=True , title= 'number of fires' , loc='center left', bbox_to_anchor=(1, 0.5) ,
+	          fancybox=True )
+
+	plt.title(f'Spatial distribution of fires in LANDIS scenario {scenario_no}' , fontsize=10 )
+
+	plot_name = 'spatial_distribution_of_fires_for_scen_'+scenario_no+'_'+my_domain+'.'+plot_format
 
 #===========================================================
-# loop through months and and filter chunks
 
-for month in month_list :
+if (spatial_plot_type=='marker') :
 
-	print( f'-> processing month= {month}' )
+ # plot for a position/place/marker
+ 	desired_underlying_map.plot( marker_lon , marker_lat , marker='.' , color='k' , latlon=True )
 
-	filter_month = ( input_df_nonZero_days[ 'FireDay-30' ] >= month_dict[month][0] ) & ( input_df_nonZero_days[ 'FireDay-30' ] <= month_dict[month][1] )
+#===========================================================
 
-	chunck_of_df = input_df_nonZero_days [ filter_month ]  
+if ( spatial_plot_type=='mesh' ) :
 
-	lat_list_of_fires = chunck_of_df ['Lat'].values
-	print(f'-> no. of burned pixels in {month} in scen ({scenario_no})= {len(lat_list_of_fires)}')
-	#print(lon_list_of_fires)
+	# plot a mesh on the map
+	dir_path='/Users/ehsan/Documents/Python_projects/USFS_fire/inputs/'
 
-	lon_list_of_fires = chunck_of_df ['Long'].values
-	#print(f'-> no. of fires in {month} = {len(lon_list_of_fires)}')
+	grid_dot_file_name='GRIDDOT2D_161001'
+	grid_cross_file_name='GRIDCRO2D_161001'
 
-	#x_coord , y_coord = my_desired_base_map( lon_list_of_fires , lon_list_of_fires ) # order: x ,y; degrees to meters
+	grid_dot_file=dir_path+grid_dot_file_name
+	grid_cross_file=dir_path+grid_cross_file_name
 
-	#my_desired_base_map.scatter( x_coord , y_coord , latlon=True , marker= month_dict[month][2] , s=10 , label=month) # If lon_list_of_fireslon_list_of_fires is False (default), x and y are assumed to be in map projection coordinates. 
-#source: https://matplotlib.org/basemap/api/basemap_api.html
+	print(f'-> GRID_DOT file is= {grid_dot_file}')
+	print(f'-> GRID_CRO file is= {grid_cross_file}')
 
-	my_desired_base_map.scatter( lon_list_of_fires , lat_list_of_fires , latlon=True , marker= month_dict[month][2] , s=12 , label=month ,  zorder=5 ) # If lon_list_of_fireslon_list_of_fires is False (default), x and y are assumed to be in map projection coordinates. 
+	grid_dot_open=Dataset( grid_dot_file , 'r')
+	grid_cross_open=Dataset( grid_cross_file , 'r')
 
-#my_desired_base_map.etopo(scale=0.5, alpha=0.5)  # the map backgroud 
-#my_desired_base_map.shadedrelief(scale=0.5)
-plt.legend( scatterpoints=1 , frameon=True , title= 'number of fires' , loc='center left', bbox_to_anchor=(1, 0.5) ,
-          fancybox=True )
+	print('-----------------------------------------')
+	print(f'-> info for file {grid_dot_file_name} is=')
+	print('-> dimension is= %s' %( str(grid_dot_open.variables['LOND'].dimensions ))) # f-string does not work for this syntax
+	print('-> shape is= %s' %str(grid_dot_open.variables['LOND'].shape )) # and this
+	print('-----------------------------------------')
+	print(f'-> info for file {grid_cross_file_name} is=')
+	print('-> dimension is= %s' %( str(grid_cross_open.variables['LON'].dimensions ))) # f-string does not work for this syntax
+	print('-> shape is= %s' %str(grid_cross_open.variables['LON'].shape )) # and this
+	print('-----------------------------------------')
 
-plt.title(f'Spatial distribution of fires in LANDIS scenario {scenario_no}' , fontsize=10 )
+	lon_dot_arr=np.array( grid_dot_open.variables['LOND'][0,0,:,:] )
+	lat_dot_arr=np.array( grid_dot_open.variables['LATD'][0,0,:,:] )
+
+	lon_cross_arr=np.array( grid_cross_open.variables['LON'][0,0,:,:] )
+	lat_cross_arr=np.array( grid_cross_open.variables['LAT'][0,0,:,:] )
+
+	# plot for different mesh type
+	if ( mesh_type=='sample_mesh' ) :
+
+		desired_underlying_map.scatter( lon_dot_arr , lat_dot_arr , marker='.' , color='b' , latlon=True , zorder=5 )
+		desired_underlying_map.scatter( lon_cross_arr , lat_cross_arr , marker='+' , color='k' , latlon=True , zorder=6 )
+
+	# plot for different mesh type
+	if ( mesh_type=='modeling_domain') :
+
+		lon_min=np.min( lon_dot_arr ) # get the min from dot arr
+		lon_max=np.max( lon_dot_arr ) # get max of dot arr
+		lat_min=np.min( lat_dot_arr ) # get min of lat dot arr
+		lat_max=np.max( lat_dot_arr ) # get max of dot lat arr
+
+		lon_coord_list=[lon_min , lon_min , lon_max , lon_max , lon_min ] # put the points in order to appear
+		lat_coord_list=[lat_min , lat_max , lat_max , lat_min , lat_min ]
+
+		desired_underlying_map.plot( lon_coord_list , lat_coord_list , color='r' , latlon=True , zorder=5 )  # for drawing line, don't use marker kwrd
+
+
+	if ( mesh_type=='station_inside_cell' ) :
+
+		lon_diff_arr=lon_cross_arr-stn_lon
+		lat_diff_arr=lat_cross_arr-stn_lat
+
+		#print(f'-> type of {lon_diff_arr} is= {type(lon_diff_arr)} ')
+
+		total_diff_arr=np.abs( lon_diff_arr ) + np.abs( lat_diff_arr )
+
+		tuple_of_row_col=np.argwhere( total_diff_arr==np.min(total_diff_arr) )[0]
+
+		print(f'-> row/col of our cell is= {tuple_of_row_col} ')
+		stn_row=tuple_of_row_col[0]
+		stn_col=tuple_of_row_col[1]
+
+		print(f'-> station row is= {stn_row}')
+		print(f'-> station row is= {stn_col}')
+
+		### get the corners of the cell that station falls inside it
+		ll_lat=lat_dot_arr[ stn_row , stn_col ]
+		ul_lat=lat_dot_arr[ stn_row+1 , stn_col ]
+		ur_lat=lat_dot_arr[ stn_row+1 , stn_col+1 ]
+		lr_lat=lat_dot_arr[ stn_row , stn_col+1 ]
+
+		ll_lon=lon_dot_arr[ stn_row , stn_col ]
+		ul_lon=lon_dot_arr[ stn_row+1 , stn_col ]
+		ur_lon=lon_dot_arr[ stn_row+1 , stn_col+1 ]
+		lr_lon=lon_dot_arr[ stn_row , stn_col+1 ]
+
+		stn_cell_lon_list=[ ll_lon , ul_lon , ur_lon , lr_lon , ll_lon ]
+		stn_cell_lat_list=[ ll_lat , ul_lat , ur_lat , lr_lat , ll_lat ]
+
+		print('-> now plot the station and cell')
+
+		desired_underlying_map.plot( stn_lon , stn_lat , marker='.' , color='k' , latlon=True , zorder=5 )
+
+		desired_underlying_map.plot( stn_cell_lon_list , stn_cell_lat_list , color='r' , latlon=True , zorder=5 )
+
+
+	if ( mesh_type=='region_for_stats' ) :
+
+		lon_diff_arr=lon_cross_arr-stats_mesh_ll_lon
+		lat_diff_arr=lat_cross_arr-stats_mesh_ll_lat
+
+		#print(f'-> type of {lon_diff_arr} is= {type(lon_diff_arr)} ')
+
+		total_diff_arr=np.abs( lon_diff_arr ) + np.abs( lat_diff_arr )
+
+		tuple_of_row_col=np.argwhere( total_diff_arr==np.min(total_diff_arr) )[0]
+
+		print(f'-> row/col of our cell is= {tuple_of_row_col} ')
+		marker_row=tuple_of_row_col[0]
+		marker_col=tuple_of_row_col[1]
+
+		print(f'-> cell row is= {marker_row}')
+		print(f'-> cell row is= {marker_col}')
+		print(f'-> now plot the mesh from the starting cell= {marker_row , marker_col} ')
+
+		for row in range( marker_row , marker_row+mesh_domain_range , 1 ) :
+			for column in range( marker_col , marker_col+mesh_domain_range , 1) :
+
+				#print(f'-> for row= {row} and col= {column}')
+				desired_underlying_map.plot( lon_cross_arr[row , column] , lat_cross_arr[row , column] , marker='.' , color='b' , latlon=True )
+
+
+
+
+
+
+
+
+
+	plot_name = 'plot_for_'+spatial_plot_type+'_'+mesh_type+'.'+plot_format
 
 #===========================================================
 # save the plot
 if (save_plot=='yes'):
 
-	plot_name = 'spatial_distribution_of_fires_scen'+scenario_no+'.'+plot_format
-
 	plot_dir = '/Users/ehsan/Documents/Python_projects/USFS_fire/inputs/landis_inputs/plots/'
 
 	saved_plot = plot_dir+plot_name
 	#extent = ax2.get_window_extent().transformed(fig.dpi_scale_trans.inverted())
-	plt.savefig(saved_plot , dpi=1200 , format=plot_format ) #, bbox_inches='tight')
+	plt.savefig(saved_plot , dpi=300 , format=plot_format ) #, bbox_inches='tight')
 
 	print(" ")
 	print(f'-> plot saved at=')
@@ -198,7 +372,7 @@ if (save_plot=='yes'):
 # show the plot
 else:
 	print(" ")
-	print(f'-> save plot is NO! so we only show it here.')
+	print(f'-> save plot is= NO! so we only show it here.')
 	plt.show() # save the plot and then show it.
 
 
